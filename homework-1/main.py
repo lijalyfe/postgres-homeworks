4c5c2d7de4575035d892a34ac233833c9e1bbdd7
lija_lyfe <lija6556@list.ru>
2023-08-14 20:43:15 +0500

    connecting to the database north

--- homework-1/main.py
+++ homework-1/main.py
@@ -11,3 +11,14 @@ customers_df = pd.read_csv("north_data/customers_data.csv")
 customers_df = customers_df[["customer_id", "company_name", "contact_name"]]
 orders_df = pd.read_csv("north_data/orders_data.csv")
 orders_df = orders_df[["order_id", "customer_id", "employee_id", "order_date", "ship_city"]]
+
+
+# Подключаемся к базе данных
+connection = psycopg2.connect(
+    dbname="north",
+    user="postgres",
+    password="2428",
+    host="localhost",
+    port="5432"
+)
+cursor = connection.cursor()
