d0da09dc8fed5cd48e64839f4430a6c21f04778b
lija_lyfe <lija6556@list.ru>
2023-08-14 19:24:17 +0500

    CREATING TABLES: employees, customers, orders

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
--- SQL-команды для создания таблиц
+CREATE TABLE customers (
+  customer_id SERIAL PRIMARY KEY,
+  company_name VARCHAR(50),
+  contact_name VARCHAR(50)
+);
+
+
+CREATE TABLE employees (
+  employee_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  title VARCHAR(50),
+  birth_date DATE,
+  notes VARCHAR(50)
+);
+
+
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  customer_id INTEGER REFERENCES customers(customer_id),
+  employee_id INTEGER REFERENCES employees(employee_id),
+  order_date DATE,
+  ship_sity TEXT
+);
+
+
