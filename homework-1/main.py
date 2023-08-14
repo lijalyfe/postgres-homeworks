d1f9b5e6cf1b346b4d6a9087b6cac049c5361e05
lija_lyfe <lija6556@list.ru>
2023-08-14 20:16:41 +0500

    uploading csv files

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,3 +4,10 @@ import pandas as pd
 import psycopg2
 from psycopg2 import sql
 
+# Загружаем CSV файлы
+employees_df = pd.read_csv("north_data/employees_data.csv")
+employees_df = employees_df[["employee_id", "first_name", "last_name", "title", "birth_date", "notes"]]
+customers_df = pd.read_csv("north_data/customers_data.csv")
+customers_df = customers_df[["customer_id", "company_name", "contact_name"]]
+orders_df = pd.read_csv("north_data/orders_data.csv")
+orders_df = orders_df[["order_id", "customer_id", "employee_id", "order_date", "ship_city"]]
