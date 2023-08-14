38c091fd5ddbf508a49f703ddd6ceb4a5a384d65
lija_lyfe <lija6556@list.ru>
2023-08-14 23:35:11 +0500

    add sql-queries to a file `alter_student.sql`

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,39 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+CREATE TABLE student (
+  student_id serial PRIMARY KEY,
+  first_name varchar,
+  last_name varchar,
+  birthday date,
+  phone varchar
+);
 
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
+ALTER TABLE student
+ADD COLUMN middle_name varchar;
 
 
 -- 3. Удалить колонку middle_name
+ALTER TABLE student
+DROP COLUMN middle_name;
 
 
 -- 4. Переименовать колонку birthday в birth_date
+ALTER TABLE student
+RENAME COLUMN birthday TO birth_date;
 
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
+ALTER TABLE student
+ALTER COLUMN phone TYPE varchar(32);
 
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES ('Иван', 'Иванов', '2000-01-01', '123-456-7890'),
+       ('Петр', 'Петров', '1999-02-02', '234-567-8901'),
+       ('Мария', 'Сидорова', '1998-03-03', '345-678-9012');
 
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
