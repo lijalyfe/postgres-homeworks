0482ba5bc226dbe77355104294e144a4d03be792
lija_lyfe <lija6556@list.ru>
2023-08-14 22:54:23 +0500

    add query for the name of the customer's company and the full name of the employee working on the order of this company, when both the customer and the employee are registered in London, and the order is delivered by United Package

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,14 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT c.company_name, CONCAT(e.first_name, ' ', e.last_name) AS employee
+FROM customers AS c
+JOIN orders AS o ON c.customer_id = o.customer_id
+JOIN employees AS e ON o.employee_id = e.employee_id
+JOIN shippers AS s ON o.ship_via = s.shipper_id
+WHERE c.city = 'London'
+AND e.city = 'London'
+AND s.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
